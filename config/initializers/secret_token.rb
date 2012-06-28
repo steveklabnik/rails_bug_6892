958f3f139f5fc043b34f17b9ced4667dff717938
Steve Klabnik <steve@steveklabnik.com>
2012-06-28 09:21:37 -0400

    Initial commit.
    Run rake assets:precompile to show the bug.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBug6892::Application.config.secret_token = '57fb79241387a1ad1ae21ca3df80f4023406930444186f4bfd5250dd8d8d92f2c5c420ed0be3062fdb5fc4ae06340b26a3c600baec35e10c36f8302693c41b1f'
